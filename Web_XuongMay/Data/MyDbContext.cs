3d6b7c0d0dcaeb64f58beb94de5e8ba7e5e9306d
TranTrachNguyen <trantrachnguyen2312@gmail.com>
2024-08-16 17:01:59 +0700

    update_2

---
+++ Web_XuongMay/Data/MyDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_XuongMay.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Cấu hình khóa chính tổng hợp cho OrderProduct
+            modelBuilder.Entity<OrderProduct>()
+                .HasKey(op => new { op.OrderId, op.ProductId });
+
+            // Cấu hình quan hệ nhiều-nhiều giữa Order và Products
+            modelBuilder.Entity<OrderProduct>()
+                .HasOne(op => op.Order)
+                .WithMany(o => o.OrderProducts)
+                .HasForeignKey(op => op.OrderId);
+
+            modelBuilder.Entity<OrderProduct>()
+                .HasOne(op => op.Product)
+                .WithMany(p => p.OrderProducts)
+                .HasForeignKey(op => op.ProductId);
+        }
+    }
+}
---
+++ Web_XuongMay/Data/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_XuongMay.Data
+{
+    [Table("Orders")]
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        // Sử dụng IEnumerable để biểu diễn tập hợp dữ liệu
+        public virtual IEnumerable<OrderProduct> OrderProducts { get; set; }
+    }
+}
