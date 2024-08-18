3f3a8b4f3cd53a79eff51e305adbd73c109d50ec
TranTrachNguyen <trantrachnguyen2312@gmail.com>
2024-08-18 09:12:20 +0700

    update_6

---
+++ Web_XuongMay/Models/OrderProductModel.cs
@@ -0,0 +1,13 @@
+﻿using Web_XuongMay.Data;
+
+namespace Web_XuongMay.Models
+{
+    public class OrderProductModel
+    {
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public Order Order { get; set; }
+        public Products Product { get; set; }
+    }
+}
